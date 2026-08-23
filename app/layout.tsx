595648f6c75472c307a5f7f6e43ec2cdf6d03ce4
rishiyee <hrishikeshvyshnavop@gmail.com>
2026-08-23 07:52:09 +0530

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Albert_Sans } from "next/font/google";
-import Script from "next/script"; // <-- YOU MISSED THIS
+import Script from "next/script";
 import "./globals.css";
 import Footer from "../components/footer";
 
@@ -24,22 +24,23 @@ export default function RootLayout({
   return (
     <html lang="en" className={albertSans.variable}>
       <head>
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-QY0L3JB4LD"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-QY0L3JB4LD');
-          `}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W8WKJDH');`}
         </Script>
       </head>
-
       <body className="font-sans antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W8WKJDH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <Footer />
       </body>
